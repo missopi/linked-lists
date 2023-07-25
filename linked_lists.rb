d7d37adaacde520b8cfd9dedf6efb9fe854f3742
missopi <missopi@me.com>
2023-07-25 17:11:54 +0100

    Define to_s

--- linked_lists.rb
+++ linked_lists.rb
@@ -38,6 +38,11 @@ class LinkedList
 
   # return node at given index
   def at(index)
+    node = @head
+    index.times do
+      node = node.next_node
+    end
+    node
   end
 
   # remove last element
@@ -47,7 +52,7 @@ class LinkedList
 
   # returns true if value in list
   def contains?(value)
-    return true if node(value) == value
+    return true if node.value == value
   end
 
   # returns index of node containing value
@@ -56,6 +61,12 @@ class LinkedList
 
   # list of values represented as string
   def to_s
+    node = @head
+    until node.nil?
+      puts "#{node.node}"
+      node = node.next_node
+    end
+    puts 'end'
   end
 end
 
