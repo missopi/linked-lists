e89a8a0977543df708ae08032ff2d668488adba5
missopi <missopi@me.com>
2023-07-25 14:31:14 +0100

    Add required methods to linked list class

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,7 +1,42 @@
 # frozen_string_literal: true
 
+# will represent the full list
 class LinkedList
+    # add new node to end
+    def append(value)
+    end
+    # add new node to begginning
+    def prepend(value)
+    end
+    # total number of nodes
+    def size
+    end
+    # returns first node
+    def head
+    end
+    # return last node
+    def tail
+    end
+    # return node at given index
+    def at(index)
+    end
+    # remove last element
+    def pop 
+    end
+    # returns true if value in list
+    def contains?(value)
+    end
+    # returns index of node containing value
+    def find(value)
+    end
+    # list of values represented as string
+    def to_s
+    end 
 end
 
+# contains value method and link to next node
 class Node
+    def value
+        next_node = nil
+    end
 end
