dfaaf75f7991e04ff20c68436444ef89c6bc3b3f
missopi <missopi@me.com>
2023-07-25 14:50:02 +0100

    Adjust Node class and add links

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,42 +1,52 @@
 # frozen_string_literal: true
 
 # will represent the full list
-class LinkedList
-    # add new node to end
-    def append(value)
-    end
-    # add new node to begginning
-    def prepend(value)
-    end
-    # total number of nodes
-    def size
-    end
-    # returns first node
-    def head
-    end
-    # return last node
-    def tail
-    end
-    # return node at given index
-    def at(index)
-    end
-    # remove last element
-    def pop 
-    end
-    # returns true if value in list
-    def contains?(value)
-    end
-    # returns index of node containing value
-    def find(value)
-    end
-    # list of values represented as string
-    def to_s
-    end 
+class LinkedList < Node
+  # add new node to end
+  def append(value)
+  end
+
+  # add new node to begginning
+  def prepend(value)
+  end
+
+  # total number of nodes
+  def size
+  end
+
+  # returns first node
+  def head
+  end
+
+  # return last node
+  def tail
+  end
+
+  # return node at given index
+  def at(index)
+  end
+
+  # remove last element
+  def pop 
+  end
+
+  # returns true if value in list
+  def contains?(value)
+  end
+
+  # returns index of node containing value
+  def find(value)
+  end
+
+  # list of values represented as string
+  def to_s
+  end
 end
 
-# contains value method and link to next node
+# represents a node in the linked list containing the value and the link to the next node
 class Node
-    def value
-        next_node = nil
-    end
+  def initialise(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
